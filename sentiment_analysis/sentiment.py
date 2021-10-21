6485e59bc2c2367829097e7ffc4534696ffd6653
Jed Pittman <jedpittman@gmail.com>
2021-10-20 21:53:33 -0400

    sentiment refactoring

--- sentiment_analysis/sentiment.py
+++ sentiment_analysis/sentiment.py
@@ -1,22 +1,39 @@
 import pandas as pd
-df = pd.read_csv('Reviews.csv')
-# print(df.head())
-
-
 import nltk
-# from nltk import WordCloud
-
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
-#nltk.download('stopwords')
-#from nltk.corpus import stopwords
-# Create stopword list:
-local_stopwords = set(STOPWORDS)
-local_stopwords.update(["br", "href"])
-textt = " ".join(review for review in df.Text)
-wordcloud = WordCloud(stopwords=local_stopwords).generate(textt)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.savefig('wordcloud11.png')
-plt.show()
+
+def getDataFrameFromPath(path:str):
+    """Load a csv from input path and return a pandas dataframe
+    
+    """
+    return pd.read_csv(path)
+
+def buildWordCloud(inputCSVPath:str) -> object:
+    """Take in an input csv and produce an outputPNGPath"""
+    df = getDataFrameFromPath(inputCSVPath)
+
+    # Create stopword list:
+    local_stopwords = set(STOPWORDS)
+    local_stopwords.update(["br", "href"])
+
+    textt = " ".join(review for review in df.Text)
+    return WordCloud(stopwords=local_stopwords).generate(textt)
+
+def saveWordcloudAsPNGAndShow(wordcloudInput: object, outputPNGPath: str):
+    """Take an input wordcloud and save as PNG and display"""
+    plt.imshow(wordcloudInput, interpolation='bilinear')
+    plt.axis("off")
+    plt.savefig(outputPNGPath)
+    plt.show()
+
+
+def mainRun():
+    """Run the logic to take a csv input and produce a sentiment wordcloud"""
+    wordcloud = buildWordCloud('test/Reviews.csv')
+    saveWordcloudAsPNGAndShow(wordcloud, 'test/output.png')
+
+
+if __name__ == "__main__":
+    mainRun()
